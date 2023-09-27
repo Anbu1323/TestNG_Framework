81540aaf7f2b30f8650634cc19e8296a1a2f9ad7
Anbu1323 <anbums808@gmail.com>
2023-09-27 16:41:12 +0530

    demo framework

---
+++ testNG_Framework/SimpleLogin.java
@@ -0,0 +1,33 @@
+package testNG_Framework;
+
+import org.testng.annotations.Test;
+
+
+public class SimpleLogin {
+  @Test
+  public void f() {
+	  chrome
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+  }
+}
